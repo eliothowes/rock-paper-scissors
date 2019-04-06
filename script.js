7d441d2b257a5f47cb7e3c4a1387770d6a951685
Eliot Howes <ejhowes@gmail.com>
2019-04-06 22:41:04 +0100

    Exercise compeleted. Needs final step to reset game.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay () {
     let computerSelection = ['rock', 'paper', 'scissors']
     return computerSelection[Math.floor(Math.random()*computerSelection.length)]
@@ -9,27 +12,54 @@ function userPlay () {
 }
 
 function singleRound () {
-    var p1 = userPlay().toLowerCase()
-    var p2 = computerPlay()
+    let p1 = userPlay().toLowerCase()
+    let p2 = computerPlay()
     if (p1 === p2 ) {
-        alert("It's a draw")
+        playerScore = playerScore + 0
+        computerScore = computerScore + 0 
+        alert(("It's a draw. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "rock" && p2 === "paper") {
-        alert("You Lose! Paper beats Rock")
+        playerScore = playerScore + 0
+        computerScore = computerScore + 1
+        alert(("You Lose! Paper beats Rock. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "rock" && p2 === "scissors") {
-        alert("You Win! Rock beats Scissors")
+        playerScore = playerScore + 1
+        computerScore = computerScore + 0 
+        alert(("You Win! Rock beats Scissors. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "paper" && p2 === "scissors") {
-        alert("You Lose! Scissors beats Rock")
+        playerScore = playerScore + 0
+        computerScore = computerScore + 1
+        alert(("You Lose! Scissors beats Rock. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "paper" && p2 === "rock") {
-        alert("You Win! Paper beats Rock")
+        playerScore = playerScore + 1
+        computerScore = computerScore + 0 
+        alert(("You Win! Paper beats Rock. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "scissors" && p2 === "rock") {
-        alert("You Lose! Rock beats Scissors")
+        playerScore = playerScore + 0
+        computerScore = computerScore + 1 
+        alert(("You Lose! Rock beats Scissors. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
     else if (p1 === "scissors" && p2 === "paper") {
-        alert("You Win! Scissors beats Paper")
+        playerScore = playerScore + 1
+        computerScore = computerScore + 0
+        alert(("You Win! Scissors beats Paper. ") + ("Your score = ") + playerScore + (". ") + ("The computer's score = ") + computerScore + ("."))
     }
 }
+
+function game () {
+    for (i = playerScore, j = computerScore; i || j <3; i++, j++) {
+        singleRound ();
+        if (playerScore === 3) {
+            alert ("You reached 3 points, you WIN!")
+        }
+        else if (computerScore === 3) {
+            alert ("The computer reached 3 points, you LOSE!")
+        }
+    }    
+}
+
